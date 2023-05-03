6c506a96792af39f57581f483cdd56ba4b750229
João Victor <jvfernandes.contato@gmail.com>
2023-05-02 23:50:47 -0300

    feat: create header component

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -2,12 +2,13 @@ import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from './assets/styles/global';
 import defaultTheme from './assets/styles/themes/default';
+import Header from './components/Header';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <h1>Olá mundo!</h1>
+      <Header />
     </ThemeProvider>
   );
 }
---
+++ fe/src/components/Header/styles.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background: #ccc;
+  height: 24px;
+`;

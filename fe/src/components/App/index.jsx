909d4ffd8fe46cda5e17aa215ffbef441156667e
João Victor <jvfernandes.contato@gmail.com>
2023-05-04 23:53:57 -0300

    feat: try to make the header responsive

--- fe/src/components/App/index.jsx
+++ fe/src/components/App/index.jsx
@@ -1,15 +1,21 @@
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from '../../assets/styles/global';
-import defaultTheme from '../../assets/styles/themes/default';
+// import defaultTheme from '../../assets/styles/themes/default';
 
 import { Container } from './styles';
 
 import Header from '../Header';
 
+export const theme = {
+  mediaQueries: {
+    tablet: '@media screen and (max-width: 767px)',
+  },
+};
+
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
 
       <Container>
--- fe/src/components/Header/styles.jsx
+++ fe/src/components/Header/styles.jsx
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background: #ccc;
-  height: 24px;
+  height: 4em;
+
+  ${(props) => props.theme.mediaQueries.tablet} {
+    height: 80%;
+  }
 `;
+
 export const Content = styled.div``;

3cff62413bb87258695c2ef344c7973aa25f3d00
João Victor <jvfernandes.contato@gmail.com>
2023-05-14 08:09:50 -0300

    fix: fix problem with resume button

--- fe/src/components/Header/styles.jsx
+++ fe/src/components/Header/styles.jsx
@@ -90,17 +90,17 @@ export const ButtonText = styled.button`
 
 export const ResumeButton = styled.button`
   background: transparent;
-  margin: 8px 16px;
-  padding: 8px 16px;
   color: ${({ theme }) => theme.secondaryColor};
   border: 1px solid ${({ theme }) => theme.secondaryColor};
+  margin: 8px 16px;
+  padding: 16px 32px;
   border-radius: 8px;
   transition: color 0.3s ease-in-out;
   transition: transform 0.3s ease-in-out;
 
   :hover {
     background: ${({ theme }) => theme.secondaryColor};
-    border: ${({ theme }) => theme.backgroundColor};
+    border: 1px solid ${({ theme }) => theme.backgroundColor};
     color: ${({ theme }) => theme.backgroundColor};
     transform: scale(1.2);
   }

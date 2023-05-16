3dd28c1d27f8066e33958438f3628c66ab70d298
João Victor <jvfernandes.contato@gmail.com>
2023-05-16 10:57:07 -0300

    feat: configuring the button to return to home page

--- fe/src/components/Pages/ErrorPage/index.jsx
+++ fe/src/components/Pages/ErrorPage/index.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Container, HomeButton } from './styles';
 import GlobalStyles from '../../../assets/styles/global';
@@ -13,11 +13,13 @@ export default function ErrorPage() {
       <GlobalStyles />
       <Container>
         <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
+        <p>Sorry, an unexpected error has occurred. Try again Later.</p>
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
-        <HomeButton>Página Inicial</HomeButton>
+        <Link to="/">
+          <HomeButton>Return</HomeButton>
+        </Link>
       </Container>
     </ThemeProvider>
   );

623a3c67a7d187f0dc9a051eaac1fd465b8176c4
João Victor <jvfernandes.contato@gmail.com>
2023-05-20 21:42:02 -0300

    feat: create index page styles

--- fe/src/Pages/Index/index.jsx
+++ fe/src/Pages/Index/index.jsx
@@ -7,6 +7,7 @@ import Start from '../Start';
 import About from '../About';
 import SocialStyledSideElement from '../../components/SocialStyledSideElement';
 import EmailStyledSideElement from '../../components/EmailStyledSideElement';
+import { Content, Pages } from './styles';
 
 export default function Index() {
   return (
@@ -15,8 +16,12 @@ export default function Index() {
       <Header />
       <SocialStyledSideElement />
       <EmailStyledSideElement />
-      <Start />
-      <About />
+      <Content>
+        <Pages>
+          <Start />
+          <About />
+        </Pages>
+      </Content>
     </ThemeProvider>
   );
 }
---
+++ fe/src/Pages/Index/styles.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  display: block;
+`;
+
+export const Pages = styled.main`
+  padding: 0px 150px;
+  counter-reset: section 0;
+  margin: 0px auto;
+  width: 100%;
+  max-width: 1600px;
+  min-height: 100vh;
+  display: block;
+`;

c80e63d0ed8b2348499db63698e1a74ea8a96c7c
João Victor <jvfernandes.contato@gmail.com>
2023-05-22 08:57:37 -0300

    refactor: turned a div into an h2 element on about page

--- fe/src/Pages/About/index.jsx
+++ fe/src/Pages/About/index.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable max-len */
-import ColorSourceCodeProTexts from '../../components/ColorSourceCodeProTexts';
 import ColorTexts from '../../components/ColorTexts';
 import MyPhoto from '../../assets/images/MyPhoto.png';
 import DoubleChevron from '../../assets/images/DoubleChevron.svg';
@@ -7,67 +6,61 @@ import {
   Border,
   ColorLayer,
   Column,
-  Container, Content, IconAndText, Photo, Technologies, Text, TextAndImage, Texts, Title,
+  Container, IconAndText, Photo, Technologies, Text, TextAndImage, Texts,
 } from './styles';
 
 export default function About() {
   return (
     <Container id="about">
-      <Content>
-        <Title>
-          <ColorSourceCodeProTexts>01.</ColorSourceCodeProTexts>
-          {' '}
-          <span>About Me</span>
-        </Title>
-        <TextAndImage>
-          <Texts>
-            <Text>
-              Hello, my name is João Victor and I like to create things on the internet. My first contact with web development was around 2015, when I decided to try to create a website with a collection of all Iron Maiden albums and songs after watching some HTML and CSS classes on YouTube. However, for various reasons, I unfortunately ended up putting all that aside to become a chemist (if regret could kill...).
-            </Text>
-            <Text>
-              A few years have passed and by the end of 2021, I regained my interest in web development, and today I have no doubts that this is what I want to do for the rest of my life. Currently, I am graduating as a Multiplatform Software Developer from the <ColorTexts>Faculty of Technology in Mauá</ColorTexts>, and I am always striving to improve my skills in the tools I use.
-            </Text>
-            <Text>
-              Here are a few technologies i&apos;ve been working with recently:
-            </Text>
-          </Texts>
-          <Photo>
-            <img src={MyPhoto} alt="I on my computer" />
-            <ColorLayer />
-            <Border />
-          </Photo>
-        </TextAndImage>
-        <Technologies>
-          <Column>
-            <IconAndText>
-              <img src={DoubleChevron} alt="arrow" />
-              JavaScript (ES6+)
-            </IconAndText>
-            <IconAndText>
-              <img src={DoubleChevron} alt="arrow" />
-              React
-            </IconAndText>
-            <IconAndText>
-              <img src={DoubleChevron} alt="arrow" />
-              React Native
-            </IconAndText>
-          </Column>
-          <Column>
-            <IconAndText>
-              <img src={DoubleChevron} alt="arrow" />
-              Express.js
-            </IconAndText>
-            <IconAndText>
-              <img src={DoubleChevron} alt="arrow" />
-              MongoDB
-            </IconAndText>
-            <IconAndText>
-              <img src={DoubleChevron} alt="arrow" />
-              WordPress
-            </IconAndText>
-          </Column>
-        </Technologies>
-      </Content>
+      <h2>About Me</h2>
+      <TextAndImage>
+        <Texts>
+          <Text>
+            Hello, my name is João Victor and I like to create things on the internet. My first contact with web development was around 2015, when I decided to try to create a website with a collection of all Iron Maiden albums and songs after watching some HTML and CSS classes on YouTube. However, for various reasons, I unfortunately ended up putting all that aside to become a chemist (if regret could kill...).
+          </Text>
+          <Text>
+            A few years have passed and by the end of 2021, I regained my interest in web development, and today I have no doubts that this is what I want to do for the rest of my life. Currently, I am graduating as a Multiplatform Software Developer from the <ColorTexts>Faculty of Technology in Mauá</ColorTexts>, and I am always striving to improve my skills in the tools I use.
+          </Text>
+          <Text>
+            Here are a few technologies i&apos;ve been working with recently:
+          </Text>
+        </Texts>
+        <Photo>
+          <img src={MyPhoto} alt="I on my computer" />
+          <ColorLayer />
+          <Border />
+        </Photo>
+      </TextAndImage>
+      <Technologies>
+        <Column>
+          <IconAndText>
+            <img src={DoubleChevron} alt="arrow" />
+            JavaScript (ES6+)
+          </IconAndText>
+          <IconAndText>
+            <img src={DoubleChevron} alt="arrow" />
+            React
+          </IconAndText>
+          <IconAndText>
+            <img src={DoubleChevron} alt="arrow" />
+            React Native
+          </IconAndText>
+        </Column>
+        <Column>
+          <IconAndText>
+            <img src={DoubleChevron} alt="arrow" />
+            Express.js
+          </IconAndText>
+          <IconAndText>
+            <img src={DoubleChevron} alt="arrow" />
+            MongoDB
+          </IconAndText>
+          <IconAndText>
+            <img src={DoubleChevron} alt="arrow" />
+            WordPress
+          </IconAndText>
+        </Column>
+      </Technologies>
     </Container>
   );
 }
--- fe/src/Pages/About/styles.jsx
+++ fe/src/Pages/About/styles.jsx
@@ -6,6 +6,46 @@ export const Container = styled.section`
   margin: 0px auto;
   padding: 100px 0px;
   -webkit-font-smoothing: antialiased;
+
+  h2 {
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    position: relative;
+    margin: 10px 0px 40px;
+    width: 100%;
+    font-size: 32px;
+    white-space: nowrap;
+    font-weight: 600;
+    line-height: 1.1;
+
+    ::before {
+      position: relative;
+      counter-increment: section 1;
+      content: "0" counter(section) ".";
+      margin-right: 10px;
+      color: ${({ theme }) => theme.tertiaryColor};
+      font-family: 'Source Code Pro', sans-serif;
+      font-size: 20px;
+      font-weight: 400;
+      white-space: nowrap;
+      line-height: 1.1;
+    }
+
+    ::after {
+      content: "";
+      display: block;
+      position: relative;
+      width: 300px;
+      height: 1px;
+      margin-left: 20px;
+      background-color: ${({ theme }) => theme.tertiaryColor};
+      font-size: 20px;
+      white-space: nowrap;
+      font-weight: 600;
+      line-height: 1.1;
+    }
+  }
 `;
 
 export const Content = styled.div`

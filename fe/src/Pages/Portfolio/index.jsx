289fca00f5892a584d0b74df866b8e891bd066ef
João Victor <jvfernandes.contato@gmail.com>
2023-05-29 06:45:49 -0300

    feat: create portfolio section

--- fe/src/Pages/Portfolio/index.jsx
+++ fe/src/Pages/Portfolio/index.jsx
@@ -1,9 +1,39 @@
-import { Container, Title } from './styles';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  ProjectsList, Title,
+  ProjectContainer,
+  ProjectContent,
+  Overline,
+  ProjectTitle,
+} from './styles';
 
 export default function Portfolio() {
   return (
     <Container id="portfolio">
       <Title>Some Things I&apos;ve Built</Title>
+      <ProjectsList>
+        <ProjectContainer>
+          <ProjectContent>
+            <>
+              <Overline>Featured Project</Overline>
+              <Link to="/#">
+                <ProjectTitle>Project 1</ProjectTitle>
+              </Link>
+            </>
+          </ProjectContent>
+        </ProjectContainer>
+        <ProjectContainer>
+          <ProjectContent>
+            <>
+              <Overline>Featured Project</Overline>
+              <Link to="/#">
+                <ProjectTitle>Project 2</ProjectTitle>
+              </Link>
+            </>
+          </ProjectContent>
+        </ProjectContainer>
+      </ProjectsList>
     </Container>
   );
 }
--- fe/src/Pages/Portfolio/styles.jsx
+++ fe/src/Pages/Portfolio/styles.jsx
@@ -43,3 +43,58 @@ export const Title = styled.h2`
     background-color: ${({ theme }) => theme.tertiaryColor};
   }
 `;
+
+export const ProjectsList = styled.ul`
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+`;
+
+export const ProjectContainer = styled.li`
+  position: relative;
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(12, 1fr);
+  -webkit-box-align: center;
+  align-items: center;
+
+  :not(:last-of-type) {
+    margin-bottom: 100px;
+  }
+`;
+
+export const ProjectContent = styled.div`
+  position: relative;
+  grid-area: 1 / 1 / -1 / 7;
+  display: block;
+
+  :nth-of-type(2n+1) {
+    grid-column: 7 / -1;
+    text-align: right;
+  }
+
+  a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.color};
+  }
+`;
+
+export const Overline = styled.p`
+  margin: 10px 0px;
+  color: ${({ theme }) => theme.tertiaryColor};
+  font-family: 'Source Code Pro', sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+export const ProjectTitle = styled.h3`
+  margin: 0px 0px 20px;
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 1.1;
+  display: block;
+
+  :hover {
+    color: ${({ theme }) => theme.tertiaryColor};
+  }
+`;

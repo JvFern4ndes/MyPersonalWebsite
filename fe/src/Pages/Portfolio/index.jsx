635bedae6d231998c109c88637b1fb2ff2cb20fc
João Victor <jvfernandes.contato@gmail.com>
2023-07-18 23:56:51 -0300

    feat: portfolio section changes

--- fe/src/Pages/Portfolio/index.jsx
+++ fe/src/Pages/Portfolio/index.jsx
@@ -65,6 +65,44 @@ export default function Portfolio() {
             </Content>
           </ProjectContent>
         </ProjectContainer>
+        <ProjectContainer>
+          <Picture>
+            <Wrapper>
+              <ImageWrapper>
+                <ImageContainer>
+                  <Link to="/#">
+                    <img src={MyPortfolioImage} alt="Project 1" />
+                  </Link>
+                </ImageContainer>
+              </ImageWrapper>
+            </Wrapper>
+          </Picture>
+          <ProjectContent>
+            <Content>
+              <Overline>Featured Project</Overline>
+              <Link to="/#">
+                <ProjectTitle>My Own Portfolio</ProjectTitle>
+              </Link>
+              <ProjectDescription>
+                <p>Development of my personal portfolio through javascript and ReactJs.</p>
+              </ProjectDescription>
+              <ProjectTechList>
+                <TechOfList>React</TechOfList>
+                <TechOfList>Javascript</TechOfList>
+                <TechOfList>CSS</TechOfList>
+                <TechOfList>HTML</TechOfList>
+              </ProjectTechList>
+              <ProjectLinks>
+                <Link to="/#">
+                  <img src={GithubIcon} alt="Github Link" />
+                </Link>
+                <Link to="/#">
+                  <img src={ProjectLink} alt="Project Link" />
+                </Link>
+              </ProjectLinks>
+            </Content>
+          </ProjectContent>
+        </ProjectContainer>
       </ProjectsList>
     </Container>
   );
